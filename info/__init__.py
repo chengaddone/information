00b680226189285cf9ef8e32b5d7622b5bf15f53
wjc <email>
2019-12-08 15:37:21 +0800

    完成项目框架的搭建

---
+++ info/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from redis import StrictRedis
+
+from config import config
+
+# 初始化数据库
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    # 加载配置
+    app.config.from_object(config[config_name])
+    # 数据库与app绑定
+    db.init_app(app)
+    # 初始化redis存储对象
+    redis_store = StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT)
+    # 开启当前项目的CSRF保护
+    CSRFProtect(app)
+    # 设置session
+    Session(app)
+    return app
--- manage.py
+++ manage.py
@@ -1,43 +1,15 @@
-from flask import Flask, session
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import CSRFProtect
-from flask_session import Session
+from flask import session
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
-import redis
+from info import create_app, db
 
-app = Flask(__name__)
-
-
-class Config(object):
-    """项目的配置"""
-    DEBUG = True
-    SECRET_KEY = "ADJKsad25*-dsof@$5098"
-    # 配置mysql数据库
-    SQLALCHEMY_DATABASE_URI = "mysql://root:940606@127.0.0.1:3306/information"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # 配置redis数据库
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-    # 配置session存储位置，存在redis中
-    SESSION_TYPE = 'redis'
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
-    # 开启session签名
-    SESSION_USER_SIGNER = True
-    # 设置session需要过期，且过期时间为7天
-    SESSION_PERMANENT = False
-    PERMANENT_SESSION_LIFETIME = 86400*7
-
-
-# 加载配置
-app.config.from_object(Config)
-# 初始化数据库
-db = SQLAlchemy(app)
-# 初始化redis存储对象
-redis_store = redis.StrictRedis(host=app.config["REDIS_HOST"], port=app.config["REDIS_PORT"])
-# 开启当前项目的CSRF保护
-CSRFProtect(app)
-# 设置session
-Session(app)
+app = create_app("development")
+# 使用manager管理app
+manager = Manager(app)
+# 引入数据库迁移，关联app与db，并添加迁移命令
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 @app.route("/")
@@ -48,4 +20,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
