bc0541a9f2772537dca1881ca631ae18c90be94f
wjc <email>
2019-12-09 21:17:44 +0800

    完成首页和网页的图标，以及上传前端样式

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,15 @@
 from . import index_blu
+from flask import render_template, current_app
 
 
 @index_blu.route("/")
 def index():
-    return "hello"
+    """网站首页"""
+    return render_template("news/index.html")
+
+
+@index_blu.route('/favicon.ico')
+def favicon():
+    """加载网页图标"""
+    # 使用send_static_file来加载静态文件
+    return current_app.send_static_file("news/favicon.ico")
